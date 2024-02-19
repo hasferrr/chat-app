2bf568e7b35f9b1df3147bc679773c73a3ed17fe
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-02-19 13:23:55 +0700

    feat(socket): implements socket.io

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,39 @@
-import { Button } from '@/components/ui/button'
 import { ModeToggle } from '@/components/mode-toggle'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
+import { useEffect, useState } from 'react'
+import { socket } from './socket'
 
 const App = () => {
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    if (!socket.connected) {
+      socket.connect()
+    }
+  }, [])
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    socket.emit('message', message)
+    setMessage('')
+  }
+
   return (
-    <div
-      className={cn(
-        'container',
-        'mx-auto',
-        'px-4',
-        'h-screen',
-        'bg-white',
-        'dark:bg-[#36393E]'
-      )}
-    >
-      <p>Hello react</p>
-      <div>
-        <Button>this is button</Button>
-      </div>
-      <div>
-        <ModeToggle />
-      </div>
+    <div className={cn('container', 'mx-auto', 'px-4', 'h-screen')}>
+      <ModeToggle />
+      <form onSubmit={handleSubmit}>
+        <div className="flex w-full max-w-sm items-center space-x-2">
+          <Input
+            type="text"
+            placeholder="type a message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <Button type="submit">Send</Button>
+        </div>
+      </form>
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const { createServer } = require('node:http')
+const { Server } = require('socket.io')
 
 const app = express()
 const server = createServer(app)
+const io = new Server(server, { cors: true })
 
 app.get('/', (req, res) => {
   res.json({ status: 'ok' })
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+  socket.on('message', (msg) => {
+    console.log(msg)
+  })
 })
 
 const PORT = 3000
