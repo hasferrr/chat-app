08630d0b678acaa2aeb57de2716f7dbbf454b677
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-02-23 14:34:50 +0700

    fix(chatUI): now, logged user's chat on the right side

--- client/src/components/Chats/Chat/Chat.tsx
+++ client/src/components/Chats/Chat/Chat.tsx
@@ -1,33 +1,44 @@
+import { useUser } from '@/context/userContext'
 import { cn } from '@/lib/utils'
 
 import { IChat } from '@/types/interfaces'
 import epochToDate from '@/utils/epochToDate'
 
 const Chat = ({ chat }: { chat: IChat }) => {
+  const user = useUser()
+
   return (
     <div
       className={cn(
-        'bg-green-200',
-        'dark:bg-green-800',
-        'w-fit',
-        'my-2',
-        'py-2',
-        'px-4',
-        'rounded-xl',
-        'rounded-bl-none',
-        'max-w-[90vw]',
-        'md:max-w-2xl',
-        'break-words'
+        'w-full flex flex-col my-2',
+        chat.author === user?.username ? 'items-end' : 'items-start'
       )}
     >
-      <div>
-        <span className="font-semibold">{chat.author}</span>
-        <span> </span>
-        <span className="opacity-60 text-sm">
-          {epochToDate(chat.epochTime)}
-        </span>
+      <div
+        className={cn(
+          'bg-green-200',
+          'dark:bg-green-800',
+          'w-fit',
+          'py-2',
+          'px-4',
+          'rounded-xl',
+          chat.author === user?.username
+            ? 'rounded-br-none'
+            : 'rounded-bl-none',
+          'max-w-[90vw]',
+          'md:max-w-2xl',
+          'break-words'
+        )}
+      >
+        <div>
+          <span className="font-semibold">{chat.author}</span>
+          <span> </span>
+          <span className="opacity-60 text-sm">
+            {epochToDate(chat.epochTime)}
+          </span>
+        </div>
+        <div>{chat.message}</div>
       </div>
-      <div>{chat.message}</div>
     </div>
   )
 }
