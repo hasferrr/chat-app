b3739b04f6bb333d5e008b2f553df2593101da50
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-02-23 18:26:24 +0700

    fix(scroll): only auto scrolls down when the scrollbar at the bottom

--- client/src/components/Chats/Chats.tsx
+++ client/src/components/Chats/Chats.tsx
@@ -1,34 +1,48 @@
-import { useEffect, useRef } from 'react'
-import { ScrollArea } from '@/components/ui/scroll-area'
+import { useEffect, useRef, useState } from 'react'
 
 import Chat from './Chat/Chat'
 import { useChats } from '@/context/chatsContext'
 
 const Chats = () => {
   const chats = useChats()
-  const chatRef = useRef<HTMLDivElement>(null)
+  const chatContainerRef = useRef<HTMLDivElement>(null)
+  const [isAtBottom, setIsAtBottom] = useState(true)
 
   useEffect(() => {
-    if (chatRef.current) {
-      chatRef.current.scrollIntoView({
-        behavior: 'smooth',
-        block: 'end',
-      })
+    const chatContainer = chatContainerRef.current
+    if (chatContainer) {
+      const handleScroll = () => {
+        const { scrollTop, clientHeight, scrollHeight } = chatContainer
+        setIsAtBottom(scrollHeight - scrollTop === clientHeight)
+      }
+
+      chatContainer.addEventListener('scroll', handleScroll)
+
+      return () => {
+        chatContainer.removeEventListener('scroll', handleScroll)
+      }
     }
-  }, [chats.length])
+  }, [])
+
+  useEffect(() => {
+    const chatContainer = chatContainerRef.current
+    if (chatContainer && isAtBottom) {
+      chatContainer.scrollTop = chatContainer.scrollHeight
+    }
+  }, [chats.length, isAtBottom])
 
   return (
-    <ScrollArea>
-      <div className="h-[78vh] pr-4">
-        {chats.map((chat, index) => (
-          <Chat
-            key={index}
-            chat={chat}
-          />
-        ))}
-        <div ref={chatRef}></div>
-      </div>
-    </ScrollArea>
+    <div
+      className="h-[78vh] pr-4 overflow-y-scroll"
+      ref={chatContainerRef}
+    >
+      {chats.map((chat, index) => (
+        <Chat
+          key={index}
+          chat={chat}
+        />
+      ))}
+    </div>
   )
 }
 
